73ba0e31fbfe57899d5ee4d60b8e6f0265b46506
cameron coxen <coxecame@isu.edu>
2020-09-13 21:22:26 -0600

    Added comments and link

--- MultiplicationTable.vb
+++ MultiplicationTable.vb
@@ -2,6 +2,7 @@
 'RCET0265
 'Fall 2020
 'Multiplication Table
+'https://github.com/CalvinAC/MultiplicationTable/blob/master/MultiplicationTable.vb
 
 Option Explicit On
 Option Strict On
@@ -11,6 +12,8 @@ Module MultiplicationTable
 
 
     Sub Main()
+        'This code takes the users value and sets it to a variable that determines the values input into the 
+        'multiplication table. 
         Dim userInput As String
         Dim row As Integer
         Dim column As Integer
@@ -18,6 +21,10 @@ Module MultiplicationTable
         Dim userNum As Integer
         Dim goodData As Boolean
 
+        'A while loop that prompts user to enter a value for the table to display,
+        'if it within the parameters of 1 to 12 it will exicute, otherwise
+        'the user is asked to enter the correct values. Also allows the user
+        'to quit the program.
         Do
 
             Console.WriteLine("Please enter a number for the Multiplication table, or select N to quit.")
@@ -30,7 +37,7 @@ Module MultiplicationTable
                 If userNum > 0 And userNum < 13 Then
                     goodData = True
 
-                ElseIf userNum > 12 Or userNum < 0 Then
+                ElseIf userNum > 12 Or userNum < 1 Then
                     Console.WriteLine("That is not between 1 and 12, please try again")
                     Console.ReadLine()
                     Console.Clear()
@@ -45,14 +52,13 @@ Module MultiplicationTable
 
         Loop Until goodData = True
 
+        'Prints the text for the table uses the users input to create the table
         Console.WriteLine("Enjoy your  " & userNum & " X " & userNum & " Multiplication Table:")
-
         For column = 1 To userNum
-
             For row = 1 To userNum
 
                 table(row, column) = row * column
-                Console.WriteLine(table(row, column) & "  ")
+                Console.Write(table(row, column) & vbTab)
 
             Next
             Console.WriteLine()
